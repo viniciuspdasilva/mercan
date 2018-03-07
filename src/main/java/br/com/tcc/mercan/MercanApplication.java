a13fdc2c690dcdbda8760a3a86f6190e000ca35d
Vinicius Silva <viniciussilva32171@gmail.com>
2018-03-07 10:09:31 -0300

    Initial commit

---
+++ src/main/java/br/com/tcc/mercan/MercanApplication.java
@@ -0,0 +1,12 @@
+package br.com.tcc.mercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercanApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/tcc/mercan/MercanApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.tcc.mercan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MercanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
